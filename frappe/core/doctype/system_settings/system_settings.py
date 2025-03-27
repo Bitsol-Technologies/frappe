607dd41abca5bd8e225ef6e0bb06ccb7a9916cf5
laibamasoodd <laiba.masood@bitsol.tech>
2025-03-27 10:49:25 +0500

    added clockify api & workspace to settings (#16)

--- frappe/core/doctype/system_settings/system_settings.py
+++ frappe/core/doctype/system_settings/system_settings.py
@@ -31,6 +31,8 @@ class SystemSettings(Document):
 		backup_limit: DF.Int
 		bypass_2fa_for_retricted_ip_users: DF.Check
 		bypass_restrict_ip_check_if_2fa_enabled: DF.Check
+		clockify_api_key: DF.Data | None
+		clockify_workspace_id: DF.SmallText | None
 		country: DF.Link | None
 		currency: DF.Link | None
 		currency_precision: DF.Literal["", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
